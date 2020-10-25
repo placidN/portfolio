69fbb389b62745589f87f7861755164c81407871
placidN <33428284+placidN@users.noreply.github.com>
2020-10-25 16:33:18 -0400

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -72,7 +72,7 @@ function sdMl(eml, name, message){
         Email.send({
             Host : "smtp.gmail.com",
             Username : "freshmarket538@gmail.com",
-            Password : "icui4cu100",
+            Password : {process.env.EMAIL_PASS}",
             To : "placidonthejob@gmail.com",
             From : "<freshmarket538@gmail.com, Contact [nwoguplacid.online]>",
             Subject : "You Were Contacted!",
